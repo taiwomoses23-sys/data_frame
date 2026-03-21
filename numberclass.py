5cf9029bd579e481f53b3f99d20baf167ba07d41
Moses Taiwo <taiwomoses23@gmail.com>
2026-03-21 19:38:21 +0000

    numberclass.py

---
+++ numberclass.py
@@ -0,0 +1,122 @@
+num_one = 7
+num_two = 2
+print(num_one * num_two)
+print(num_one + num_two)
+print(type(num_one))
+print(type(num_two))
+ans = num_one / num_two
+print(ans)
+ans2 = num_one // num_two
+print(ans2)
+print(type(ans2))
+print(type(ans))
+# modulus operator % " displays the reminder after the decimal in a float number " , assignment =
+ans3 = num_one % num_two
+print(ans3)
+print(type(ans3))
+min = -5
+# abs() absolute number, convert negative number to positive/whole number
+abs(min)
+print(abs(min))
+pw = 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2
+print(pw)
+pw_2 = 2 ** 8
+print(pw_2)
+
+import random
+
+student = ["nuel", "joy", "mike", "Esther", "john"]
+name = random.choice(student)
+print(name)
+
+print(random.sample(student, 3))
+
+
+# from nuel gift hub
+num1 = 20
+num2 = 20.57777
+
+num3 =num1 + num2
+print(num3)
+
+num4 = 2 + 3j
+num5 = 4 + 5j
+print(num4 + num5)
+
+print(num5 + num1)
+
+num_1, num_2 = 5, 10
+sub = num_2 - num_1
+
+num_one = 7
+num_two = 2
+print(num_one / num_two)
+print(type(num1))
+print(type(num2))
+print(type(num4))
+
+num_one * num_two
+
+print(num4 * num5)
+
+print(num_one * num_two)
+print(num_two + num_one)
+
+name = "Mary"
+print(type(name))
+
+
+num_one = 70
+num_two = 2
+ans = num_one / num_two
+print(ans)
+
+ans2 = num_one // num_two
+print(ans2)
+
+ans3 = num_one % num_two
+print(ans3)
+
+min = -5
+print(abs(min))
+pw = 2 * 2 *2* 2*2*2*2*2
+print(pw)
+pw_2 = 2 ** 8
+print(pw_2)
+print(5**5)
+
+print(pow(2,8))
+
+new_num = 3.789
+rd = round(new_num, 1)
+print(rd)
+num = [3,5,6,7] #list
+print(max(4,6,8,60,87))
+#min()
+
+print(sum(num))
+print(sum([8,90,3,50]))
+#print(min([7,90,3,97]))
+
+import math
+
+n1 = 4.9999
+ceil = math.ceil(n1)
+print(ceil)
+flr = math.floor(n1)
+print(flr)
+
+import random
+rnd = random.random()
+print(rnd)
+ten  = random.randint(100000,999000)
+print(ten)
+random_1 = round(random.random() *10000)
+print(random_1)
+
+students = ['Nuel','Joy','Mike','Esther','John']
+name = random.choice(students)
+print(name)
+print(random.sample(students,3))
+
+
