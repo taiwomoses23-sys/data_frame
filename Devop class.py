31eb0faa47ec764644c028f8dffb8274d61f4e18
Moses Taiwo <taiwomoses23@gmail.com>
2026-03-26 17:24:05 +0000

    Devop class.py

---
+++ Devop class.py
@@ -0,0 +1,18 @@
+# DAY 1
+# Devops is a process of increasing/or improving organizational delivery of applications in a quick manner by ensuring proper automation,good quality maintained, continuous monitoring/observation and continuous testing.
+# End goal of an organization is quick delivery and no manual process required and ensuring the above important concepts.
+# CI/CD Continuous integration and continuous delivery
+# How to improve delivery of Devops: this involves automations(company perspective), quality(customers perspective),Monitoring, testing
+# Why devops: To improve efficient delivery of Apps ensuring automation, maintain quality, continuous observation an continuous testing
+#Interesting Terms: CCR= centralized Code Repository, Builder Release Engineer: deploys application on the app server
+# How to introduce yourself as a devops: I am a devops engineer, i have overall 5-6 years of experience in devops,
+# and before that i was working as a system admin or as a build and release engineer or as a server administrator.
+# then explain your responsibilities in those past roles and your current organization: in my current organization i take care of automations,
+# I ensure good quality is maintained, set up continuous monitoring and i have automated the testing process into the devops lifecycle.
+# ou can talk about the tools and technologies you're using: For the CICD i am using Github action, for the deployment of application onto container orchestration i am using kubernetes or for configuration management using ansible,
+# or Infrastructure automation using  terraform.
+# As a fresher, I just started learning about devops, I was passionate about devops...
+# AWS cloud server for administration.
+
+# DAY 2 Software developer lifecycle
+#
---
+++ march21class.py
@@ -0,0 +1,109 @@
+print(3/2)
+print(3//2)
+
+import math
+print(math.floor(3.5))
+
+print (5/2)
+print (5%2) #modulus operator % provide the reminder in a float
+print(4 ** 2)
+print(4 * 4)
+
+ # <, <=, >, >= operators
+print(5 < 2)
+print(5<=2)
+num = 5000
+num2 = 3000
+print(num < num2)
+print(num > num2)
+print(num == num2)
+tax = 20
+print(num >= num2)
+print (5 > 5)
+print (5==5)
+print(5 >= 5)
+num3 = 10
+num4 = "10"
+print(num3 == num4)
+
+password = 'n003er'
+passwor1= 'n003er'
+
+print(password == passwor1)
+# == comparative operator between conditions
+
+print(3 != 4)
+print(3 == 4)
+print (2 != 4)
+
+# "&" "| = or"
+ # when using "and" both sides must be true but when you use "or" one side has to be true
+print(2 < 5 and 3 <= 3)
+print(3 <= 2 or 3 <= 1)
+
+username = "moses"
+password = "m001a"
+
+username1 = "moses"
+password2 = "m001a"
+
+#if username1 != username:
+     # print("username incorrect")
+   #  else
+    # print("username correct")
+
+n = 0
+if not n:
+    print("n is empty")
+else:
+    print("n is not empty")
+
+name = ""
+vote_age = 18
+
+if not vote_age:
+    print("yes, u can vote")
+
+a = 5
+print (a)
+# if a > 3:
+   # a += 5
+   # print(a)
+
+a = 5
+a = a + 20
+print (a)
+#if password == password2 and username == username1:
+   # if email == email2 and firstname == firstname:
+      #  if bal = <= amount and if account_type =="saving"
+
+# input() everything input here has to be strings
+
+username = input("enter your username:")
+print (username)
+password = input("enter your password:")
+print (password)
+age = input("enter your age:")
+print(age)
+print(type(age))
+n = 10
+print(n << 2)
+# type casting: converting one data type to another
+
+num = 20.5
+int(num)
+print(num)
+str()
+print(type(num))
+price = input("enter your price:")
+print(price)
+price =+ 5
+str(price)
+
+
+
+
+
+
+
+
